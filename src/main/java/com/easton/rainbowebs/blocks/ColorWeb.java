2b7f8e55e3c25340dee13f73b47eacc1663bd165
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-01-09 01:55:48 -0500

    Ported orange, pink, purple, and yellow cobwebs
    to forge 1.16.4

--- src/main/java/com/easton/rainbowebs/blocks/ColorWeb.java
+++ src/main/java/com/easton/rainbowebs/blocks/ColorWeb.java
@@ -4,16 +4,16 @@ package com.easton.rainbowebs.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.WebBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.ShearsItem;
 import net.minecraftforge.common.ToolType;
 
 public class ColorWeb extends WebBlock {
 
     public ColorWeb() {
         super(Block.Properties.create(Material.WEB)
-                .hardnessAndResistance(4.0F)
-                .harvestLevel(0)
+                .hardnessAndResistance(4.0F,4.0F)
+                .harvestLevel(20)
                 .doesNotBlockMovement()
-                .notSolid()
-                .harvestTool(ToolType.PICKAXE));
+                .notSolid());
     }
 }
