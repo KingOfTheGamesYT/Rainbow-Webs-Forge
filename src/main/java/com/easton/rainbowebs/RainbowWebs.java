ee372cb4b19fed9f205277a7fd0a0b49d72692fd
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2024-02-18 16:58:15 -0500

    initial workspace
    ported to 1.20.1

--- src/main/java/com/easton/rainbowebs/RainbowWebs.java
+++ src/main/java/com/easton/rainbowebs/RainbowWebs.java
@@ -1,8 +1,6 @@
 package com.easton.rainbowebs;
 
-
 import com.easton.rainbowebs.util.RegistryHandler;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,18 +11,13 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("rainbowebs")
 public class RainbowWebs {
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "rainbowebs";
 
     public RainbowWebs() {
 
-        // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
@@ -57,7 +50,6 @@ public class RainbowWebs {
         RenderTypeLookup.setRenderLayer(RegistryHandler.PINK_COBWEB.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(RegistryHandler.PURPLE_COBWEB.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(RegistryHandler.YELLOW_COBWEB.get(), RenderType.getCutout());
-
     }
 }
 
