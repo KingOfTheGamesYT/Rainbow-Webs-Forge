d0eb9b0ecfbfaeb52ee971425f690cc126860be2
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2024-09-26 20:08:13 -0400

    Now this mod is fully ported
    to 1.20.1

--- src/main/java/com/easton/rainbowebs/blocks/ColorWeb.java
+++ src/main/java/com/easton/rainbowebs/blocks/ColorWeb.java
@@ -3,22 +3,16 @@ package com.easton.rainbowebs.blocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShearsItem;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.WebBlock;
-
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import net.minecraftforge.common.IForgeShearable;
 
-import java.util.Collections;
-import java.util.List;
-
-public class ColorWeb extends WebBlock implements IForgeShearable {
+public class ColorWeb extends WebBlock {
 
     public ColorWeb() {
         super(Block.Properties.copy(Blocks.COBWEB)
@@ -27,9 +21,17 @@ public class ColorWeb extends WebBlock implements IForgeShearable {
     }
     @Override
     public float getDestroyProgress(BlockState state, Player player, BlockGetter worldIn, BlockPos pos) {
-        if (player.getMainHandItem().getItem() instanceof SwordItem) {
-            return 1.0F; // Instant break with sword
+        if (player.getMainHandItem().getItem() instanceof SwordItem || player.getMainHandItem().getItem() instanceof ShearsItem) {
+            return 0.1F; // Instant break with sword
         }
         return super.getDestroyProgress(state, player, worldIn, pos);
+    }
+    @Override
+    public void playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
+        ItemStack heldItem = player.getMainHandItem();
+        if (heldItem.getItem() instanceof SwordItem || heldItem.getItem() instanceof ShearsItem) {
+            level.destroyBlock(pos, true);  //Drops the block with normal drops
+        }
+        super.playerWillDestroy(level, pos, state, player);
     }
     }
